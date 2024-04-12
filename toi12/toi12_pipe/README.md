de7f8713e083b8d44e7a8aaa414acc08992822ae
Krist Ponpairin <krist7599555@gmail.com>
2024-04-13 01:18:03 +0700

    feat: add toi12 pipe

--- toi12/toi12_pipe/README.md
+++ toi12/toi12_pipe/README.md
@@ -5,5 +5,11 @@
 
 [💎 problem.pdf](./toi12_pipe.pdf)
 
+[🎉 solution.cpp](./toi12_pipe.cpp)
+
 <img width="700" src="https://github.com/krist7599555/toi/assets/19445033/80c80822-7583-4bcd-a705-dae3eacdee85" />
 <!-- @codegen_problem end -->
+
+# Solution
+
+Just **Minimum Spanning Tree**
---
+++ toi12/toi12_pipe/toi12_pipe.cpp
@@ -0,0 +1,51 @@
+#include <cassert>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <tuple>
+#include <array>
+using namespace std;
+const int MAXN = 15009;
+const int INF = 1e9;
+
+struct Point { int x; int y; };
+inline int point_distance(const Point& lhs, const Point& rhs) {
+  return std::abs(lhs.x - rhs.x) + std::abs(lhs.y - rhs.y);
+}
+
+bool visit[MAXN]; // !pure global array is faster than std::vector<bool> so the grader will pass
+int dist[MAXN]; //
+
+int main() {
+  std::cin.sync_with_stdio(0);
+  std::cin.tie(0);
+  
+  int n, k; std::cin >> n >> k;
+  std::vector<Point> point(n);
+  for (auto& p : point) std::cin >> p.x >> p.y;
+  
+  std::fill_n(visit, n, false);
+  std::fill_n(dist, n, INF);
+  std::vector<int> mst;
+  
+  for (int node = 0; mst.size() != n - 1;) {
+    std::tuple<int, int> local_min = { INF, -1 }; // [distance, node]
+    visit[node] = true;
+    for (int nx = 0; nx < n; nx++) {
+      if (visit[nx]) continue;
+      int cost = point_distance(point[node], point[nx]);
+      local_min = std::min(local_min, {
+        dist[nx] = std::min(dist[nx], cost),
+        nx
+      });
+    }
+    auto [min_cost, ans_node] = local_min;
+    mst.push_back(min_cost);
+    node = ans_node;
+    assert(node != -1);
+  }
+  std::sort(mst.begin(), mst.end());
+  int ans = std::accumulate(mst.begin(), mst.begin() + n - k, 0);
+  std::cout << ans << '\n';
+}
