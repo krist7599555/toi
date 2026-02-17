1106c5cc92de10cb0e2303edaed699e55b6071e3
Krist Ponpairin <krist7599555@gmail.com>
2026-02-17 12:53:08 +0700

    feat: new test cli

---
+++ test.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+set -e
+
+WORKDIR="${1:-.}"
+
+find $WORKDIR -type f -name '*.cpp' | sort -V | while read -r cpp_file; do
+  # printf "AT $cpp_file: ";
+  printf "%-60s" "AT $cpp_file "
+  
+  cpp_dir="$(dirname "$cpp_file")"
+
+  clang++ -std=c++20 "$cpp_file" -o "./tmp/bin"
+  
+  found=false
+
+  for test_case in $cpp_dir/testcase/*.in; do
+    [ -e "$test_case" ] || continue
+    name="$(basename "$test_case" .${test_case##*.})"
+    found=true
+    ./tmp/bin < "$test_case" > "./tmp/$name.out"
+    git diff --no-index --color=always --quiet \
+      "${test_case%.in}.sol" "./tmp/$name.out"
+    # echo "  Pass - $test_case"
+    printf "P"
+  done
+  
+  if [ "$found" = false ]; then
+  printf "\033[90m[SKIP]\033[0m\n"
+else
+  printf " \033[32m[PASS]\033[0m\n"
+fi
+done
+echo \033[32mFinished\033[0m
